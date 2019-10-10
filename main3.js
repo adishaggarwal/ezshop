8a96e66b7c17cc37a2c558cda1ea6a7247b484bd
adish aggarwal <aggarwaladish@gmail.com>
2019-10-10 12:43:49 +0530

    vewc

---
+++ main3.js
@@ -0,0 +1,13 @@
+var p4more=document.getElementById("p4more");
+var p4transactions=document.getElementById("p4transactions");
+var p4rules=document.getElementById("p4rules");
+var p4support=document.getElementById("p4support");
+var p4signout=document.getElementById("p4signout");
+
+p4more.addEventListener('click',function1);
+
+function function1(e)
+{
+    window.location.href='title.html';
+}
+
