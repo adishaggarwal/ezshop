2600d1c3f16123d57516d13832d2b62fa80ce21c
adish aggarwal <aggarwaladish@gmail.com>
2019-10-11 17:31:26 +0530

    c dfb

---
+++ main6.js
@@ -0,0 +1,16 @@
+var supportbtn=document.getElementById("supportbtn");
+var reportbtn=document.getElementById("reportbtn");
+
+supportbtn.addEventListener('click',ff1);
+
+function ff1(e)
+{
+    window.location.href='more.html';
+}
+
+reportbtn.addEventListener('click',ff2);
+
+function ff2(e)
+{
+    window.location.href='report.html';
+}
