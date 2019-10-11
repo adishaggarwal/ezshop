3902f4e6cf6cc8a48667a78cc487687186f549c7
adish aggarwal <aggarwaladish@gmail.com>
2019-10-11 10:45:38 +0530

    kjnkl

--- main3.js
+++ main3.js
@@ -3,6 +3,10 @@ var p4transactions=document.getElementById("p4transactions");
 var p4rules=document.getElementById("p4rules");
 var p4support=document.getElementById("p4support");
 var p4signout=document.getElementById("p4signout");
+var modal9yes=document.getElementById("modal9yes");
+var modal9no=document.getElementById("modal9no");
+var modal9=document.getElementById("modal9");
+var nrdiv=document.getElementById("nrdiv");
 
 p4more.addEventListener('click',function1);
 
@@ -10,4 +14,35 @@ function function1(e)
 {
     window.location.href='title.html';
 }
+
+p4signout.addEventListener('click',function2);
+
+function function2(e)
+{
+    modal9.style.display='block';
+}
+
+modal9yes.addEventListener('click',function3);
+
+function function3(e)
+{
+    modal9.style.display='none';
+    window.location.href='index.html';
+}
+
+modal9no.addEventListener('click',function4);
+
+function function4(e)
+{
+    modal9.style.display='none';
+}
+
+p4transactions.addEventListener('click',function5);
+
+function function5(e)
+{
+   
+    window.location.href='noresults.html';
+}
+
 
